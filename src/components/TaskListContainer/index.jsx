6bbd3f50b324079dc067bb992ac69fe559121835
Sudhardeena <sudharsanam16499@gmail.com>
2024-11-23 10:02:34 +0530

    added empty view for all tabs

--- src/components/TaskListContainer/index.jsx
+++ src/components/TaskListContainer/index.jsx
@@ -7,12 +7,16 @@ const TaskListContainer = () => {
     const taskContext = useContext(TaskContext)
     const {taskList,activeStatusTab} = taskContext
     const filterdTaskList = taskList.filter(each=>(each.status===activeStatusTab || activeStatusTab==='All'))
-
+  
   return (
     <ul className='task-list'>
-        {filterdTaskList.map(each=>
-            <TaskItem key={each.taskId} taskItemDetails={each}/>
-        )}
+        {
+            filterdTaskList>0?
+            filterdTaskList.map(each=>
+                <TaskItem key={each.taskId} taskItemDetails={each}/>
+            ):
+            <img className="empty-img" src="https://res.cloudinary.com/da9omg4ab/image/upload/v1731616509/empty-concept-illustration_114360-1188_xhayir.webp" />
+        }
     </ul>
   )
 }
